ce49b7ac8fc4654254b999adbae1686a78e50865
wpforit1 <wpforit1@gmail.com>
2025-02-04 16:26:29 +0530

    Initial commit

---
+++ DB/db_config.py
@@ -0,0 +1,8 @@
+# db_config.py
+DB_CONFIG = {
+    "username": "postgres",
+    "password": "1020",
+    "host": "localhost",
+    "port": 5432,
+    "database": "postgres",
+}
---
+++ DB/db_service.py
@@ -0,0 +1,34 @@
+# db_service.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from contextlib import contextmanager
+from db_config import DB_CONFIG
+
+class DBService:
+    def __init__(self):
+        self.engine = create_engine(
+            f"postgresql://{DB_CONFIG['username']}:{DB_CONFIG['password']}@"
+            f"{DB_CONFIG['host']}:{DB_CONFIG['port']}/{DB_CONFIG['database']}"
+        )
+        self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+
+    def get_engine(self):
+        """Get the SQLAlchemy engine."""
+        return self.engine
+
+    @contextmanager
+    def get_session(self):
+        """Provide a transactional scope."""
+        session = self.SessionLocal()
+        try:
+            yield session
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            raise e
+        finally:
+            session.close()
+
+
+# # Create a singleton instance of DBService
+# db_service = DBService()
